7217729dd244ec7993af31ec8a69067099c13d38
abnuo <66645994+abnuo@users.noreply.github.com>
2020-09-20 06:09:54 +0100

    Delete source.java

--- source.java
+++
@@ -1,9 +0,0 @@
-class HelloWorld {
-
-    public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            double rand = Math.random(); 
-        System.out.println("Hello, World");
-        }
-    }
-}
